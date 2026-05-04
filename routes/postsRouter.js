d61b964d85982d2bc157cbd2c4692e9677077e94
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-05-04 14:10:46 +0100

    chore: add footer partial to views and structure layout and forms

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -8,7 +8,7 @@ const { isAuth, isAdmin } = require("./authMiddleware");
 
 router.use(isAuth);
 router.get("/feeds{/:p}", allPosts);
-router.get("/:userid/create-post", getNewPost);
-router.post("/:userid/create-post", postNewPost);
+router.get("/create-post", getNewPost);
+router.post("/create-post", postNewPost);
 
 module.exports = router;
